846f6df84f36fb7a63867bb83ac6b9e0e0925f55
Adrià Ribas <aribas.dam@institutcampalans.net>
2026-03-10 15:28:05 +0100

    canvis

--- resources/views/pistes/layout.blade.php
+++ resources/views/pistes/layout.blade.php
@@ -15,8 +15,7 @@
     <!-- Linkejem css de app.css -->
         @vite('resources/css/app.css')
 
-
-
+        
     <!-- Lato Lletra -->
 
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,53 @@
 <style>
 body {
   background-color: rgb(49, 49, 49);
+  display: flex;
+  justify-content: center;
+  align-items: center;   
+  height: 100vh;          
+  margin-right: 0;              
 }
+
+button {
+  padding: 10px 20px;
+  text-transform: uppercase;
+  border-radius: 8px;
+  font-size: 17px;
+  font-weight: 500;
+  color: #ffffff80;
+  text-shadow: none;
+  background: transparent;
+  cursor: pointer;
+  box-shadow: transparent;
+  border: 1px solid #ffffff80;
+  transition: 0.5s ease;
+  user-select: none;
+}
+
+#btn:hover,
+:focus {
+  color: #ffffff;
+  background: #008cff;
+  border: 1px solid #008cff;
+  text-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 20px #ffffff;
+  box-shadow: 0 0 5px #008cff, 0 0 20px #008cff, 0 0 50px #008cff,
+    0 0 100px #008cff;
+}
+
+.botons {
+}
+
 </style>
 </head>
 
 <body>
+<div class=".botons">
 
-  <a href= "{{ route('login') }}">Pistes</a>
+  <a href= "{{ route('login') }}">  <button id="btn">Login</button></a>
+    <a href= "{{ route('login') }}">  <button id="btn">Register*</button></a>
 
+</div>
 </body>
-</html>
+
+
+</html> 
